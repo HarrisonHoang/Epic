bbdd02754069360314d11ab15116856a8d2fdfbf
ToanHo8 <113567066+ToanHo8@users.noreply.github.com>
2022-10-23 11:19:47 -0400

    Newest update Sunday for epic 5

--- SWE_Epic5.py
+++ SWE_Epic5.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 #Global Variables
 user = "" 
 Request = {}
@@ -62,24 +65,150 @@ def additionalOptions():
 # Edit: additionalOption(), profile()
 def profile():
     while user: #while user has a value, aka the user is logged in
-        print("\n\nPROFILE\n\n")
+        option = str(input("Do you want to create(c), view(v), edit(e) your profile? "))
 
-        #If user created profile already, then read from userprofile.txt
-        lines = open("userprofile.txt", "r").readlines()
-        #updatedLines = []
-        for line in lines:
-            splitLine = line.split()
-            if user == splitLine[0]:
-                print("\nUser:", splitLine[0], "\n")
-                print("\nTitle:", splitLine[1], "\n")
-                print("\nMajor:", splitLine[2], "\n")
-                print("\nAt University: \n")
-                print("\nAbout: (paragraph): \n")
-                print("\nExperience: (up to 3 past jobs): \n")
-                print("\nEducation: \n")
-                #updatedLines.append(line)
-        
-        #If user have NOT create or want to edit profile now, then write/append txt.
+        if option == 'c':
+            #input value
+            #name = str(input("Enter name: "))
+            title = str(input("Enter title: ")) #1 line of text
+            major = str(input("Enter major: ")) #converted first letter upper and rest lower
+            major = ' '.join(elem.capitalize() for elem in major.split())
+            univer = str(input("Enter university: ")) #converted first letter upper and rest lower
+            univer = ' '.join(elem.capitalize() for elem in univer.split())
+            about = str(input("Enter About me: ")) #1 paragraph of text
+            print("For experience, you can insert up to 3 past jobs. If none, enter 0" + 
+                "(include title, employer, data started and ended, location, and describe what you did")
+            experi1 = str(input("Enter lastest experience: "))
+            experi2 = str(input("Enter 2nd experience: "))
+            experi3 = str(input("Enter 3th experience: "))
+            edu = str(input("Enter education (include school name, degree, and years attended: "))
+
+            #first row contain the title each column
+            myheaders = ['name', 'title', 'major', 'univer', 'about', 'experi1', 'experi2', 'experi3', 'edu']
+            #data value rows of csv file
+            myvalue = [{'name': user, 'title': title, 'major': major, 'univer': univer, 'about': about, 'experi1': experi1, 'experi2': experi2, 'experi3': experi3, 'edu': edu}]
+
+            #open a csv file and append value in, auto close when done
+            with open('profile.csv', 'a') as file:
+                csv_writer = csv.DictWriter(file, fieldnames=myheaders)
+                check_file = os.stat('profile.csv').st_size  #read csv file to see if it is empty
+                if(check_file == 0):        #check if csv file is empty
+                    csv_writer.writeheader()    #add myheaders (1st row in)
+                    csv_writer.writerows(myvalue)
+                else:
+                    csv_writer.writerows(myvalue)
+
+        if option == 'v':
+            #open a csv file and read value within it, auto close when done
+            with open('profile.csv', 'r') as file:
+                csv_reader = csv.DictReader(file)
+                for line in csv_reader:
+                    if line['name'] == user:
+                        print("\nProfile of: " + line['name'])
+                        print("Title: " + line['title'])
+                        print("Major: " + line['major'])
+                        print("University: " + line['univer'])
+                        print("About me: " + line['about'])
+                        print("Experience 1: " + line['experi1'])
+                        print("Experience 2: " + line['experi2'])
+                        print("Experience 3: " + line['experi3'])
+                        print("Education: " + line['edu'])
+
+        if option == 'e':
+            #open a csv file and read value within it, auto close when done
+            with open('profile.csv', 'r+') as file:
+                csv_reader = csv.DictReader(file)
+                for line in csv_reader:
+                    if line['name'] == user:
+                        print("\nProfile of: " + line['name'])
+                        print("Note: Press 'e' if edit, press k to keep. ")
+                        
+                        print("Title: " + line['title'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            title = str(input("Enter title: "))
+                        #if choice == 'k':
+                        else:
+                            #continue
+                            title = line['title']
+
+                        print("Major: " + line['major'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            major = str(input("Enter major: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            major = line['major']
+
+                        print("University: " + line['univer'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            univer = str(input("Enter univer: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            univer = line['univer']
+
+                        print("About me: " + line['about'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            about = str(input("Enter about: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            about = line['about']
+
+                        print("Experience 1: " + line['experi1'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            experi1 = str(input("Enter experi1: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            experi1 = line['experi1']
+
+                        print("Experience 2: " + line['experi2'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            experi2 = str(input("Enter experi2: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            experi2 = line['experi2']
+
+                        print("Experience 3: " + line['experi3'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            experi3 = str(input("Enter experi3: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            experi3 = line['experi3']
+
+                        print("Education: " + line['edu'])
+                        choice = (str(input("Edit[e] or keep[k]")))
+                        if choice == 'e':
+                            edu = str(input("Enter edu: "))
+                        #if choice == 'k':
+                        else:
+                            #break
+                            edu = line['edu']
+                    break
+
+                #first row contain the title each column
+                myheaders = ['name', 'title', 'major', 'univer', 'about', 'experi1', 'experi2', 'experi3', 'edu']
+
+                #data value rows of csv file
+                myvalue = [{'name': user, 'title': title, 'major': major, 'univer': univer, 'about': about, 'experi1': experi1, 'experi2': experi2, 'experi3': experi3, 'edu': edu}]
+
+                csv_writer = csv.DictWriter(file, fieldnames=myheaders)
+                csv_writer.writerows(myvalue)
+
+
+        if option == 'x':
+            General()
+        print("\nPress (x) to quit\n")
 
 
 def network(user):
@@ -298,18 +427,10 @@ def CreateAcc():
 
         if len(newPass)>7 and len(newPass)<13 and upper and digit and specChar:   
             #after username and password are valid, ask for first and last name, and store new user password, first name, and last name in file          
-            firstName = str(input("\nPlease enter your first name: "))
-            lastName = str(input("\nPlease enter your last name: "))
-            college = str(input("\nPlease enter the college you attend: "))
-            college = ' '.join(elem.capitalize() for elem in college.split())
-
-            major = str(input("\nPlease enter your major: "))
-            major = ' '.join(elem.capitalize() for elem in major.split())
-            
-            title = str(input("\n Please enter your title for your account: "))
-            about = str(input("\nPlease enter about yourself: "))
-            experiance = str(input("\nPlease your last three job experiance: "))
-
+            firstName = str(input("\nPlease enter your first name \n"))
+            lastName = str(input("\nPlease enter your last name\n"))
+            College = str(input("\nPlease enter the college you attend \n"))
+            major = str(input("\nPlease enter your major\n"))
             
 
             file = open("userfile.txt", "a")
@@ -321,7 +442,7 @@ def CreateAcc():
             file.write(" ")
             file.write(lastName)
             file.write(" ")
-            file.write(college)
+            file.write(College)
             file.write(" ")
             file.write(major)
             file.write("\n")
