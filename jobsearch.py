4490536a08f7cb00145aefb6c998a512c2f67520
ToanHo8 <113567066+ToanHo8@users.noreply.github.com>
2022-10-28 22:59:21 -0400

    Friday commit to main

--- jobsearch.py
+++ jobsearch.py
@@ -1,8 +1,22 @@
-
+def printJobDetail():
+    with open('jobfile.txt', 'r') as file:
+            #jobList = file.readlines()
+            #print(jobList)
+            data = file.read()
+            data_into_list = data.split("\n")       #put value in txt into a list
+            #print(data_into_list)
+            for i in range(len(data_into_list)):    #search name of user in that list
+                if data_into_list[i] == user:
+                    print(data_into_list[i])
+                    print(data_into_list[i+1])
+                    print(data_into_list[i+2])
+                    print(data_into_list[i+3])
+                    print(data_into_list[i+4])
+                    print(data_into_list[i+5])
 
 
 while True: #Keep going until a valid option is put
-    user = "toan"
+    user = "hays"
 
     NewJobPost =str(input("\nDo you want to post(p), delete(d) your post or search(s) job/interships: "))
 
@@ -12,7 +26,7 @@ while True: #Keep going until a valid option is put
         employer = input("Employer:\n")
         location = input("Location\n")
         salary = input("Salary:\n")
-
+        status = " "
         #file = open("jobfile.txt", "a")
         with open("jobfile.txt", "a") as file:
             file.write(user)
@@ -27,6 +41,8 @@ while True: #Keep going until a valid option is put
             file.write("\n")
             file.write(salary)
             file.write("\n")
+            file.write(status)  #auto status = blank
+            file.write("\n")
             
             countJob = 0
             with open(r"jobfile.txt", 'r') as file:  #read each job line save in txt
@@ -38,22 +54,61 @@ while True: #Keep going until a valid option is put
 
 
     elif NewJobPost == "S" or "s":
-        print("All jobs currently in system:")
 
         with open('jobfile.txt', 'r') as file:
             #jobList = file.readlines()
             #print(jobList)
             data = file.read()
             data_into_list = data.split("\n")       #put value in txt into a list
+            print("All jobs currently in system:")
             print(data_into_list)
+            
+            selAJob = str(input("Select the job you want by enter its title ('intership' for example): "))
             for i in range(len(data_into_list)):    #search name of user in that list
-                if data_into_list[i] == user:
-                    print(data_into_list[i])
-                    print(data_into_list[i+1])
-                    print(data_into_list[i+2])
-                    print(data_into_list[i+3])
-                    print(data_into_list[i+4])
-                    print(data_into_list[i+5])
+                if data_into_list[i] == selAJob:
+                    print("Title: " + data_into_list[i])
+                    print("Description: " + data_into_list[i+1])
+                    print("Employer: " + data_into_list[i+2])
+                    print("Location: " + data_into_list[i+3])
+                    print("Salary: " + data_into_list[i+4])
+
+                    aOrS = str(input("Do you want to apply now (a) or (s) save/unsave for later? "))
+                    if aOrS == 'a':
+                        if data_into_list[i-1] == user: #the user is the one posted
+                            print('Cannot apply for a job that you posted')
+                        if data_into_list[i+5] == ' ':
+                            grad_date = str(input("Enter grad date (mm/dd/yyyy): "))
+                            start_date = str(input("Enter date start working (mm/dd/yyyy): "))
+                            paragraph = str(input("Enter paragraph answer why you would be a good fit for this job: "))
+                            #with open('jobfile.txt', 'w') as file:
+                                #data_into_list[i+5] = 'applied'     #change status to 'applied'
+                                #file.write
+                                #print(data_into_list[i+5])
+                        if data_into_list[i+5] == 'applied':
+                            print('You applied this already')
+                    
+                    if aOrS == 's':
+                        if data_into_list[i+5] == 'saved': #saved already
+                            sORu = str(input("You saved this post already. Do you want to unsave (u) or keep it (any key): "))
+                            if sORu == 'u': 
+                                print("change status to saved")
+                                ##with open('jobfile.txt', 'w') as file:
+                                    #data_into_list[i+5] = 'saved'     #change status to 'applied'
+                                    #file.write
+                                    #print(data_into_list[i+5])
+                            else:
+                                print("Saved!")
+                        if data_into_list[i+5] == ' ': #new job to save
+                            print("change status to saved")
+                            ##with open('jobfile.txt', 'w') as file:
+                                #data_into_list[i+5] = 'saved'     #change status to 'applied'
+                                #file.write
+                                #print(data_into_list[i+5])
+                        if data_into_list[i+5] == 'applied' or data_into_list[i-1] == user: #new job to save
+                            print("Cannot save a job you posted or applied")
+                        
+                        
+
 
             
         break
@@ -62,6 +117,7 @@ while True: #Keep going until a valid option is put
 
     elif NewJobPost == "D" or "d":
         print("delete a job that you posted")
+        printJobDetail()
         break
 
 
@@ -71,3 +127,4 @@ while True: #Keep going until a valid option is put
     else:
         print("\nPress (x) to quit\n")
         break
+
